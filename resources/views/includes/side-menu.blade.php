c77cf90178d8bab61aa807a892ff41abbcd2e665
dizzyjay123 <dizzyjay12>
2020-03-07 01:54:56 +0200

    Made template layouts

---
+++ resources/views/includes/side-menu.blade.php
@@ -0,0 +1,32 @@
+<div class="welcome-menu">
+    <a href="/" class="logo">
+      <img
+        src="/img/images.png"
+        alt="">
+    </a>
+    <div class="menu">
+      <div class="menu-title">
+        Menu
+      </div>
+      <ul class="links">
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/waitlist">WaitLists</a></li>
+        <li><a href="/offers">Offers</a></li>
+        <li><a href="/giftcards">GiftCards</a></li>
+      </ul>
+    </div>
+    <div class="social-icons">
+      <a href="#"><i class="fa fa-facebook-official"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-instagram"></i></a>
+    </div>
+    <div class="location">
+      <div class="address">
+        Ssnit flats, <br>
+        Asuoyeboah- Kumasi 12353
+      </div>
+      <div class="phone-number">
+        <a href="tel:7744158565">7744158565</a>
+      </div>
+    </div>
+  </div>
